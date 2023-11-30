ef7bc9aa08bcdbe11cc20505bde0d6e2a8d8d26d
ivrez <reznichivan@gmail.com>
2023-11-30 22:39:38 +0300

    make this correct

--- lab02-trusting-trust/step1/quine-gen.c
+++ lab02-trusting-trust/step1/quine-gen.c
@@ -2,11 +2,23 @@
 
 int main() {
     FILE *file = stdin;  // stdin for input file
-    char line[1024];
 
-    while (fgets(line, sizeof(line), file) != NULL) {
-        printf("%s", line);
+    int maxArraySize = 512;
+    char realProg[maxArraySize];
+
+    int i = 0;
+    int ch;
+
+    while ((ch = fgetc(file)) != EOF && i < maxArraySize - 1) {
+        realProg[i++] = ch;
     }
+    realProg[i] = '\0';
+
+    printf("char prog[] = {\n");
+	for(i = 0; realProg[i]; i++)
+		printf("\t%d,%c", realProg[i], (i+1)%8==0 ? '\n' : ' ');
+    printf("0 };\n");
+    printf("%s", realProg);
 
     return 0;
 }
