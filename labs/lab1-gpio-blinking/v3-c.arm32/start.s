5b630ef03067cb0a1e306f6a8f7f36212ada947c
ivrez <reznichivan@gmail.com>
2023-12-15 14:52:58 +0300

    some changes

--- labs/lab1-gpio-blinking/v3-c.arm32/start.s
+++ labs/lab1-gpio-blinking/v3-c.arm32/start.s
@@ -1,19 +1,12 @@
 /* start.s */
-.section .text.startup
+.section .text.bootload
 .globl _start
 
 _start:
-    /* Set up the stack pointer */
+    /* set up the stack pointer */
     ldr sp, =0x8000
 
-    /* Call the main function */
+    /* call the main function */
     bl main
 
-    /* Halt the system */
-    b .
-
-/* Define a dummy IRQ handler to prevent undefined reference */
-.section .text
-.globl __dummy_irq_handler
-__dummy_irq_handler:
-    b __dummy_irq_handler
+hang: b hang /* infinite loop */
